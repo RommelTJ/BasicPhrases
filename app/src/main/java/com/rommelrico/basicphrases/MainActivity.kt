33e56b432066ce2501e6be7911bacbaff3252b45
Rommel Rico <me@rommelrico.com>
2018-11-02 08:34:45 -0700

    Creating the playPhrase function

--- app/src/main/java/com/rommelrico/basicphrases/MainActivity.kt
+++ app/src/main/java/com/rommelrico/basicphrases/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.rommelrico.basicphrases
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    fun playPhrase(view: View) {
+        val buttonPressed = view as Button
+        val mediaPlayer = MediaPlayer.create(this, resources.getIdentifier(buttonPressed.tag.toString(), "raw", packageName))
+        mediaPlayer.start()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
